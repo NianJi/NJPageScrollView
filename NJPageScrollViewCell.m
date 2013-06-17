d735019626e7eb3f6980b55f43c4662c7e7a2c5c
liukun <765409243@qq.com>
2013-06-18 03:55:00 +0800

    codes

---
+++ NJPageScrollViewCell.m
@@ -0,0 +1,35 @@
+//
+//  NJPageScrollViewCell.m
+//  SuningEBuy
+//
+//  Created by 刘坤 on 12-4-25.
+//  Copyright (c) 2012年 Suning. All rights reserved.
+//
+
+#import "NJPageScrollViewCell.h"
+
+@implementation NJPageScrollViewCell
+
+@synthesize reuseIdentifier = _reuseIdentifier;
+
+- (void)dealloc {
+    [_reuseIdentifier release]; _reuseIdentifier = nil;
+    [super dealloc];
+}
+
+- (id)initWithReuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super init];
+    
+    if (self) {
+        self.reuseIdentifier = reuseIdentifier;
+    }
+    return self;
+}
+
+- (void)prepareForReuse
+{
+    
+}
+
+@end
